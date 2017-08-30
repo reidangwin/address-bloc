65ace385d9a6bdbea25142eb2261a8ec5c8c9b10
Reid Angwin <rangwin@gmail.com>
2017-08-30 05:46:06 -0400

    7 Address Bloc: Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -33,4 +33,26 @@ class AddressBook
     target_index = entries.find_index { |entry| entry.name == name && entry.phone_number == phone_number && entry.email == email }
     entries.delete_at(target_index)
   end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+
+  end
+
 end
